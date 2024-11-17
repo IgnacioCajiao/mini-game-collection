b0cfca7b7b1c6d8cc4c0f00ad19c90fb202f1bc9
Ignacio Cajiao <000930213@mohawkcollege.ca>
2024-11-17 01:52:25 -0500

    Add namespace to tag script

--- unity-project/mini-game-collection/Assets/2024/Team16/Scripts/PlayerTag.cs
+++ unity-project/mini-game-collection/Assets/2024/Team16/Scripts/PlayerTag.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTag : MonoBehaviour
+namespace MiniGameCollection.Games2024.Team16
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerTag : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
